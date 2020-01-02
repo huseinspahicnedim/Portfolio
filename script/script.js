0fe0c94954c718fa2f3d749a59c74774b5da0529
huseinspahicnedim <43785284+huseinspahicnedim@users.noreply.github.com>
2020-01-02 16:33:57 +0100

    Portfolio v3
    New look for my portfolio .

--- script/script.js
+++ script/script.js
@@ -1,71 +1,84 @@
+// Smooth scroll
 
-$('a[href*="#"]')
-  
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      
-      if (target.length) {
-        
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) {
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); 
-            $target.focus(); 
-          };
-        });
-      }
-    }
+$(document).ready(function() {
+
+  var headerHeight = $('header').outerHeight(); // Target your header navigation here
+  console.log (headerHeight);
+
+  $('#main-nav li a').click(function(e) {
+
+  	var targetHref   = $(this).attr('href');
+
+	$('html, body').animate({
+		scrollTop: $(targetHref).offset().top - 80 // Add header height
+	}, 1000);
+
+    e.preventDefault();
   });
+});
 
+// //Hamburger menu toggler
+//
+// $(document).ready(function(){
+//     $(".menu-icon").on("click", function(){
+//         $("nav ul").toggleClass("showing");
+//     });
+// });
+//
+// //Scrolling sticky navbar
 
-  whatAnimation("fadescroll");
-  whatAnimation("moveleft");
+$(window).on("scroll", function(){
+  if($(window).scrollTop()) {
+    $("nav").addClass("grey");
+  }
+  else {
+    $("nav").removeClass("grey");
+  }
+});
 
-   $(window).scroll(function(){
-   	  whatAnimation("fadescroll");
-   	  whatAnimation("moveleft");
-   });
+//Link closes Hamburger menu
 
-   function whatAnimation(name) {
-   		$("." + name).each(function(){
-   		  switch (name) {
-   		  	case "fadescroll": AddClass(this, "fade");
-   		  	break;
-   		  	case "moveleft": AddClass(this, "left");
-   		  	break;
-   		  
-   		  }	
-   		});
-   };
-
-   function AddClass (object, name) {
-   	if(IsVisible(object)) {
-   		$(object).addClass(name);
-   	} else {
-   	$(object).removeClass(name);
-   	}
-   }
-
-   function IsVisible(object) {
-   	var viewport = $(window).scrollTop() + $(window).height();
-   	var rand = $(object).offset();
-   	rand.bottom = rand.top + $(object).outerHeight();
-   	return(!(viewport < (rand.top + 500) || $(window).scrollTop() > (rand.bottom - 250)));
-   }
+// $(document).ready(function(){
+//     $(".linkToggler").on("click", function(){
+//         $("nav ul").toggleClass("showing");
+//     });
+// });
+//
+//
+//
+//
+//   // Fade in Animation
+//
+//   window.sr = ScrollReveal();
+//
+//
+//   sr.reveal('.lineReveal', {duration: 2700});
+//   sr.reveal('#centralContent', {duration: 2700});
+//   sr.reveal('#Prodaja', {duration: 2700});
+//   sr.reveal('#Geodezija', {duration: 2700});
+//   sr.reveal('#Ovlastenja', {duration: 2700});
+//   sr.reveal('#Ekanaliza', {duration: 2700});
+//   sr.reveal('#edukacija', {duration: 2700});
+//   sr.reveal('#medij', {duration: 2700});
+//   sr.reveal('#kontaktpage', {duration: 2700});
+//
+//
+//
+// // Turns jquery off on Mobile
+//
+// if($(window).width() < 1024){
+//   sr.reveal('#Geodezija', {duration: 0});
+//   sr.reveal('#Prodaja', {duration: 0});
+//   sr.reveal('#Ovlastenja', {duration: 0});
+//   sr.reveal('#Ekanaliza', {duration: 0});
+//   sr.reveal('#edukacija', {duration: 0});
+//   sr.reveal('#medij', {duration: 0});
+//   sr.reveal('#kontaktpage', {duration: 0});
+//   sr.reveal('.lineReveal', {duration: 0});
+// }
+//
+// $(document).ready(function() {
+// window.onload = function () {
+// $('#loader').fadeOut(500, function(){ $('#loader').remove(); } );
+// }
+// });
